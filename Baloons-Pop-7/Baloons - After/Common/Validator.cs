197c0a4662d63a2ed4af0c4dc06a9f9d1bc6bd66
pkeremidchieva <p.keremidchieva@gmail.com>
2015-09-28 20:31:27 +0300

    Revert "Empty classes for Print/Validator"
    This reverts commit 28d7ed37b433a5f5d8b85b865a153fb2ff4beded.

--- Baloons-Pop-7/Baloons - After/Common/Validator.cs
+++ Baloons-Pop-7/Baloons - After/Common/Validator.cs
@@ -1,5 +1,10 @@
-﻿
-namespace Baloons.Common
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baloons___After.Common
 {
     class Validator
     {
--- Baloons-Pop-7/Baloons - After/UI/PrintMenu.cs
+++ Baloons-Pop-7/Baloons - After/UI/PrintMenu.cs
@@ -1,5 +1,10 @@
-﻿
-namespace Baloons.UI
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baloons___After.UI
 {
     class PrintMenu
     {
