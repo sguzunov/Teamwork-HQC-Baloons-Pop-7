ba2d6a218144d5a3a2f83017a9390df0ca8cc443
pkeremidchieva <p.keremidchieva@gmail.com>
2015-09-28 22:01:17 +0300

    temp variable is named better

--- Baloons-Pop-7/Baloons - After/Commands/RemoveBallonsCommand.cs
+++ Baloons-Pop-7/Baloons - After/Commands/RemoveBallonsCommand.cs
@@ -10,24 +10,24 @@
         internal static void RemovePoppedBaloons(string[,] gameField)
         {
             int i;
-            Queue<string> temp = new Queue<string>();
+            Queue<string> currentBaloon = new Queue<string>();
             for (int j = GameConstants.HEIGHT_OF_FIELD - 1; j >= 0; j--)
             {
                 for (i = GameConstants.WIDTH_OF_FIELD - 1; i >= 0; i--)
                 {
                     if (gameField[i, j] != ".")
                     {
-                        temp.Enqueue(gameField[i, j]);
+                        currentBaloon.Enqueue(gameField[i, j]);
                         gameField[i, j] = ".";
                     }
                 }
                 i = 4;
-                while (temp.Count > 0)
+                while (currentBaloon.Count > 0)
                 {
-                    gameField[i, j] = temp.Dequeue();
+                    gameField[i, j] = currentBaloon.Dequeue();
                     i--;
                 }
-                temp.Clear();
+                currentBaloon.Clear();
             }
         }
     }
