b03db8b32d141dd19f54e368910f878d7d2482be
pkeremidchieva <p.keremidchieva@gmail.com>
2015-09-28 23:40:59 +0300

    Now Change of baloons colors is separated

--- Baloons-Pop-7/Baloons - After/UI/ChangeBaloonColor.cs
+++ Baloons-Pop-7/Baloons - After/UI/ChangeBaloonColor.cs
@@ -5,35 +5,30 @@ namespace Balloons.ConsoleUI
 
     class PrintMenu
     {
-        public static void DrawBaloonsInColor(string currentNumberToChange)
+        public static char DrawBaloonsInColor(string currentNumberToChange)
         {
             char symbol = '\u00A4';
             switch (currentNumberToChange)
             {
                 case "1":
                     Console.ForegroundColor = ConsoleColor.Blue;
-                    Console.Write(currentNumberToChange = symbol + " ");
                     break;
                 case "2":
                     Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write(currentNumberToChange = symbol + " ");
                     break;
                 case "3":
                     Console.ForegroundColor = ConsoleColor.Green;
-                    Console.Write(currentNumberToChange = symbol + " ");
                     break;
                 case "4":
                     Console.ForegroundColor = ConsoleColor.Yellow;
-                    Console.Write(currentNumberToChange = symbol + " ");
                     break;
                 case "5":
                     Console.ForegroundColor = ConsoleColor.DarkMagenta;
-                    Console.Write(currentNumberToChange = symbol + " ");
                     break;
                 default:
                     break;
-
             }
+            return symbol;
         }
     }
 }
