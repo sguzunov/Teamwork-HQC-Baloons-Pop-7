6ddfd9fbdd56a150af3fec3110e7b951c53ad662
pkeremidchieva <p.keremidchieva@gmail.com>
2015-09-28 19:42:17 +0300

    Empty classes for Print/Validator

---
+++ Baloons-Pop-7/Baloons - After/UI/PrintMenu.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Baloons___After.UI
+{
+    class PrintMenu
+    {
+    }
+}
