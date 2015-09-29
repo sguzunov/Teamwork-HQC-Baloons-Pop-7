00c25533810776d4a754700af47b2e6231c599b8
pkeremidchieva <p.keremidchieva@gmail.com>
2015-09-29 15:31:49 +0300

    Some validations
    Not connected to the code

---
+++ Baloons-Pop-7/Baloons - After/Common/Validators.cs
@@ -0,0 +1,24 @@
+﻿
+namespace Baloons.Common
+{
+    using System;
+
+    public static class Validator
+    {
+        public static void CheckIfNameIsEmpty(string name)  // Commands.StatisticsCommand
+        {
+            if (name == " ")
+            {
+                throw new NullReferenceException("Name can not be empty");
+            }
+        }
+        public static void CheckLenghtOfName(string name) // Commands.StatisticsCommand
+        {
+            if (name.Length > 3 || name.Length <= 10)
+            {
+                throw new IndexOutOfRangeException("Name must be between 3 and 10 chars");
+            }
+        }
+
+    }
+}
