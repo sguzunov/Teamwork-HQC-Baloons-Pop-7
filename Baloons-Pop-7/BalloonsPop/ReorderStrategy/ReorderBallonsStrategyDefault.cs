c7907a672fe6d8429f45cbf62aa956092886ec60
medeaohm <nia_omerska@abv.bg>
2015-10-10 17:49:24 +0300

    removed unnecessary code

--- Baloons-Pop-7/BalloonsPop/ReorderStrategy/ReorderBallonsStrategyDefault.cs
+++ Baloons-Pop-7/BalloonsPop/ReorderStrategy/ReorderBallonsStrategyDefault.cs
@@ -20,32 +20,6 @@
                     }
                 }
             }
-
-            // int row;
-            // int col;
-
-            // Queue<Balloon> currentGameField = new Queue<Balloon>();
-
-            // for (col = gameField.Columns - 1; col >= 0; col--)
-            // {
-            //     for (row = gameField.Rows - 1; row >= 0; row--)
-            //     {
-            //         if (!(gameField[row, col] is BalloonPoped))
-            //         {
-            //             currentGameField.Enqueue(gameField[row, col]);
-            //             gameField[row, col] = new BalloonPoped();
-            //         }
-            //     }
-
-            //     row = gameField.Rows - 1;
-            //     while (currentGameField.Count > 0)
-            //     {
-            //         gameField[row, col] = currentGameField.Dequeue();
-            //         row--;
-            //     }
-
-            //     currentGameField.Clear();
-            // }
         }
 
         private void SwapWithNextUnPopedBalloon(IGameField gameField, int activeRow, int activeCol, int rows)
--- Baloons-Pop-7/BalloonsPop/ReorderStrategy/ReorderBallonsStrategyFly.cs
+++ Baloons-Pop-7/BalloonsPop/ReorderStrategy/ReorderBallonsStrategyFly.cs
@@ -20,32 +20,6 @@
                     }
                 }
             }
-
-            // int row;
-            // int col;
-
-            // Queue<Balloon> currentGameField = new Queue<Balloon>();
-
-            // for (col = 0; col < gameField.Columns; col++)
-            // {
-            //     for (row = 0; row < gameField.Rows; row++)
-            //     {
-            //         if (!(gameField[row, col] is BalloonPoped))
-            //         {
-            //             currentGameField.Enqueue(gameField[row, col]);
-            //             gameField[row, col] = new BalloonPoped();
-            //         }
-            //     }
-
-            //     row = 0;
-            //     while (currentGameField.Count > 0)
-            //     {
-            //         gameField[row, col] = currentGameField.Dequeue();
-            //         row++;
-            //     }
-
-            //     currentGameField.Clear();
-            // }
         }
 
         private void SwapWithNextUnPopedBalloon(IGameField gameField, int activeRow, int activeCol, int rows)
