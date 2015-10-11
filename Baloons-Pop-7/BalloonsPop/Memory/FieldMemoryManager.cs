3560b61fe9981249282cad3dfdd0a96e797799c6
sguzunov <sguzunov@gmail.com>
2015-10-11 18:57:04 +0300

    Memory tests added.

--- Baloons-Pop-7/BalloonsPop/Memory/FieldMemoryManager.cs
+++ Baloons-Pop-7/BalloonsPop/Memory/FieldMemoryManager.cs
@@ -2,19 +2,32 @@
 {
     using System;
 
+    using Balloons.Common;
     using Balloons.FieldFactory.Field;
 
     public class FieldMemoryManager : IFieldMemoryManager
     {
+        private const string GameFieldNullErrorMessage = "Game field cannot be null! ";
+
         private FieldMemory Memory { get; set; }
 
         public void Save(IMemorable gameField)
         {
+            if (ObjectValidator.IsGameObjectNull(gameField))
+            {
+                throw new ArgumentNullException(GameFieldNullErrorMessage);
+            }
+
             this.Memory = gameField.SaveField();
         }
 
         public void Undo(IMemorable gameField)
         {
+            if (ObjectValidator.IsGameObjectNull(gameField))
+            {
+                throw new ArgumentNullException(GameFieldNullErrorMessage);
+            }
+
             gameField.RestoreField(this.Memory);
         }
     }
---
+++ Baloons-Pop-7/TestBalloonsPopGame/MemoryTests/FieldMemoryManagerTests.cs
@@ -0,0 +1,53 @@
+﻿namespace TestBalloonsPopGame.MemoryTests
+{
+    using System;
+    using Balloons.FieldFactory.Field;
+    using Balloons.Memory;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Balloons.Cell;
+    using Moq;
+
+    [TestClass]
+    public class FieldMemoryManagerTests
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PassedNullGameFieldToSaveMethodShouldThrowExceptio()
+        {
+            var manager = new FieldMemoryManager();
+            manager.Save(null);
+        }
+
+        [TestMethod]
+        public void ManagerSaveMethodShouldCallGameFieldSaveFieldMethod()
+        {
+            var matrix = new Balloon[2, 2];
+            var mockField = new Mock<IGameField>();
+            mockField.Setup(f => f.SaveField()).Returns(new FieldMemory(matrix));
+
+            var manager = new FieldMemoryManager();
+
+            var field = mockField.Object;
+            manager.Save(field);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void PassedNullGameFieldToUndoMethodShouldThrowExceptio()
+        {
+            var manager = new FieldMemoryManager();
+            manager.Undo(null);
+        }
+
+        [TestMethod]
+        public void ManagerUndoMethodShouldCallGameFieldRestoreFieldMethod()
+        {
+            var mockField = new Mock<IGameField>();
+            var manager = new FieldMemoryManager();
+            var field = mockField.Object;
+
+            manager.Undo(field);
+            mockField.Verify(f => f.RestoreField(It.IsAny<FieldMemory>()), Times.AtLeastOnce());
+        }
+    }
+}
