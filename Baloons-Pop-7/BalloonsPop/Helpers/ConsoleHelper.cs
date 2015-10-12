94e829b68def2a75fe8a534dbdbb9e100f9d3437
sguzunov <sguzunov@gmail.com>
2015-10-12 16:41:59 +0300

    Added documentaticode documentation for some classes.

--- Baloons-Pop-7/BalloonsPop/Helpers/ConsoleHelper.cs
+++ Baloons-Pop-7/BalloonsPop/Helpers/ConsoleHelper.cs
@@ -6,11 +6,18 @@
     using Balloons.Common;
     using Balloons.Common.ConsoleContext;
 
+    /// <summary>
+    /// Helper class provides methods for easy working with console.
+    /// </summary>
     public static class ConsoleHelper
     {
         private const string ColorErrorMessage = "This balloon color does not exists!";
         private const string TextLenghtErrorMessage = "Text length must be bigger than 0 and less than console width!";
 
+        /// <summary>
+        /// Depending on text width and console window width centralize cursor.
+        /// </summary>
+        /// <param name="textLength">Length of the text for printing.</param>
         public static void CentraliseCursor(int textLength)
         {
             if (textLength <= 0 || Console.WindowWidth <= textLength)
@@ -23,6 +30,10 @@
             Console.SetCursorPosition(leftPosition, topPosition);
         }
 
+        /// <summary>
+        /// Sets the foreground color depending the needed one.
+        /// </summary>
+        /// <param name="color">The color the needs to be set.</param>
         public static void ChangeForegroundColorDependingOnSymbol(BalloonsColors color)
         {
             switch (color)
@@ -47,6 +58,11 @@
             }
         }
 
+        /// <summary>
+        /// A method which knows how exactly to render error messages.
+        /// </summary>
+        /// <param name="message">Message that needs to be printed.</param>
+        /// <param name="consoleWriter">Console context for rendering the message.</param>
         public static void DisplayInputErrorMessage(string message, IConsoleWriter consoleWriter)
         {
             ClearConsoleLine(consoleWriter);
@@ -57,11 +73,18 @@
             ClearConsoleLine(consoleWriter);
         }
 
+        /// <summary>
+        /// Clears whole console window.
+        /// </summary>
         public static void ClearConsole()
         {
             Console.Clear();
         }
 
+        /// <summary>
+        /// Clears the exact line the cursor is.
+        /// </summary>
+        /// <param name="consoleWriter">Console context.</param>
         private static void ClearConsoleLine(IConsoleWriter consoleWriter)
         {
             var currentLine = 1;
--- Baloons-Pop-7/BalloonsPop/Memory/FieldMemoryManager.cs
+++ Baloons-Pop-7/BalloonsPop/Memory/FieldMemoryManager.cs
@@ -5,12 +5,19 @@
     using Balloons.Common;
     using Balloons.FieldFactory.Field;
 
+    /// <summary>
+    /// This class manages field saving and restoring.
+    /// </summary>
     public class FieldMemoryManager : IFieldMemoryManager
     {
         private const string GameFieldNullErrorMessage = "Game field cannot be null! ";
 
         private FieldMemory Memory { get; set; }
 
+        /// <summary>
+        /// Method which manages saving field.
+        /// </summary>
+        /// <param name="gameField">The field to be saved.</param>
         public void Save(IMemorable gameField)
         {
             if (ObjectValidator.IsGameObjectNull(gameField))
@@ -21,6 +28,10 @@
             this.Memory = gameField.SaveField();
         }
 
+        /// <summary>
+        /// Method which manages restoring field.
+        /// </summary>
+        /// <param name="gameField">The field to be restored.</param>
         public void Undo(IMemorable gameField)
         {
             if (ObjectValidator.IsGameObjectNull(gameField))
